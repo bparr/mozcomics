29ad80e381ab488880c5547dcf1b4bc2c2aeb4d7
bparr@bparr.com <bparr@bparr.com@2768dc32-545a-11de-9272-49eb38e5b1ca>
2009-10-21 04:13:38 +0000

    Change firstRun url

--- resource/utils.js
+++ resource/utils.js
@@ -10,7 +10,7 @@ var EXPORTED_SYMBOLS = ["Utils"];
  */
 var Utils = new function() {
 	this.URLS = {
-		FIRST_RUN: "http://www.mozcomics.com/",
+		FIRST_RUN: "http://www.mozcomics.com/firstRun",
 		COMIC_LIST: "http://www.mozcomics.com/browse/",
 		UPDATE: "http://www.mozcomics.com/update?"
 	};
